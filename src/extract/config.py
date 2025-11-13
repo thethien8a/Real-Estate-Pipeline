953df9a0cda15312f0420c3646dff04c846f7aaa
Thien <thethien1708@gmail.com>
2025-11-13 23:15:40 +0700

    safe scrape

--- src/extract/config.py
+++ src/extract/config.py
@@ -9,10 +9,10 @@ class CrawlConfig:
     SUBPAGE_SEMAPHORE_LIMIT = 10
     
     # Trang bắt đầu thu thập
-    START_PAGE = 20
+    START_PAGE = 1
     
     # Trang kết thúc thu thập
-    END_PAGE = 30
+    END_PAGE = 3
 
 
 def get_page_semaphore():
