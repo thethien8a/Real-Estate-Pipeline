40bfa220ffd4f9b1bf1fb37b0ada15b24b2dd8b9
Thien <thethien1708@gmail.com>
2025-11-14 23:17:46 +0700

    run pipeline again

--- src/extract/config.py
+++ src/extract/config.py
@@ -3,7 +3,7 @@ import asyncio
 
 class CrawlConfig:
     # Semaphore cho main page - giới hạn số lượng main page được xử lý đồng thời
-    PAGE_SEMAPHORE_LIMIT = 3
+    PAGE_SEMAPHORE_LIMIT = 4
     
     # Semaphore cho subpage - giới hạn số lượng subpage được xử lý đồng thời cho mỗi main page
     SUBPAGE_SEMAPHORE_LIMIT = 10
