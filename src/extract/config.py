73bc1a1754be13a84bf4c40b033261d73c2e3c6c
Thien <thethien1708@gmail.com>
2025-11-16 12:58:37 +0700

    try again

--- src/extract/config.py
+++ src/extract/config.py
@@ -4,13 +4,13 @@ import asyncio
 class CrawlConfig:
     
     # Semaphore cho subpage - giới hạn số lượng subpage được xử lý đồng thời cho mỗi main page
-    SUBPAGE_SEMAPHORE_LIMIT = 15
+    SUBPAGE_SEMAPHORE_LIMIT = 10
     
     # Trang bắt đầu thu thập
-    START_PAGE = 200
+    START_PAGE = 90
      
     # Trang kết thúc thu thập
-    END_PAGE = 300
+    END_PAGE = 100
     
     BROWSER_ARGS = [
     '--disable-dev-shm-usage',
