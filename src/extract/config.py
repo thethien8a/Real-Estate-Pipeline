a8fdb9645b8062aa48a5d59f30d6fbcecdc00177
Thien <thethien1708@gmail.com>
2025-11-17 14:21:50 +0700

    0 to 50 pages day by day

--- src/extract/config.py
+++ src/extract/config.py
@@ -7,10 +7,10 @@ class CrawlConfig:
     SUBPAGE_SEMAPHORE_LIMIT = 10
     
     # Trang bắt đầu thu thập
-    START_PAGE = 300
+    START_PAGE = 0
      
     # Trang kết thúc thu thập
-    END_PAGE = 350
+    END_PAGE = 50
     
     BROWSER_ARGS = [
     '--disable-dev-shm-usage',
