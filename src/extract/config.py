b28ced97148047822e49fc525c39f91b2ad83885
Thien <thethien1708@gmail.com>
2025-11-17 14:50:51 +0700

    try 100 pages crawl

--- src/extract/config.py
+++ src/extract/config.py
@@ -7,10 +7,10 @@ class CrawlConfig:
     SUBPAGE_SEMAPHORE_LIMIT = 10
     
     # Trang bắt đầu thu thập
-    START_PAGE = 0
+    START_PAGE = 700
      
     # Trang kết thúc thu thập
-    END_PAGE = 50
+    END_PAGE = 800
     
     BROWSER_ARGS = [
     '--disable-dev-shm-usage',
