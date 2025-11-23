1d839ccd0af66e6a4a37be93fd86db7d610368d0
Thien <thethien1708@gmail.com>
2025-11-23 21:20:26 +0700

    1401 to 1430

--- src/extract/config.py
+++ src/extract/config.py
@@ -7,13 +7,13 @@ class CrawlConfig:
     SUBPAGE_SEMAPHORE_LIMIT = 10
     
     # Trang bắt đầu thu thập
-    START_PAGE = 1361
+    START_PAGE = 1401
     
     # Chunk size cho subpage
     SUBPAGE_CHUNK_SIZE = 200
     
     # Trang kết thúc thu thập
-    END_PAGE = 1400
+    END_PAGE = 1430
     
     BROWSER_ARGS = [
     '--disable-dev-shm-usage',
