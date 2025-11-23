e25b02c718a9432cb2da68cd8db4032f46193275
Thien <thethien1708@gmail.com>
2025-11-23 19:54:31 +0700

    update pipeline

--- src/extract/config.py
+++ src/extract/config.py
@@ -7,13 +7,13 @@ class CrawlConfig:
     SUBPAGE_SEMAPHORE_LIMIT = 10
     
     # Trang bắt đầu thu thập
-    START_PAGE = 1300
+    START_PAGE = 1331
     
     # Chunk size cho subpage
     SUBPAGE_CHUNK_SIZE = 200
     
     # Trang kết thúc thu thập
-    END_PAGE = 1330
+    END_PAGE = 1360
     
     BROWSER_ARGS = [
     '--disable-dev-shm-usage',
